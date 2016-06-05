6ddbb5a0ff693df49ec133b4255e922abee60541
lbxx0804 <lbxx0804@github.com>
2016-06-05 21:12:06 +0800

    初始化数据库

---
+++ MvcmusicStore/Models/MusicStoreDB.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MvcmusicStore.Models
+{
+    public class MusicStoreDB : DbContext
+    {
+        public MusicStoreDB()
+            : base("name=DefaultConnection")
+        { 
+        }
+
+        public IDbSet<Album> Albums { get; set; }
+
+        public IDbSet<Genre> Genres { get; set; }
+
+        public IDbSet<Artist> Artists { get; set; }
+    }
+}
